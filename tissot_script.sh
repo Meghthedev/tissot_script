74ec7ac5f7fc18c737230926567a86e2122db13f
Meghthedev <96943033+Meghthedev@users.noreply.github.com>
2022-09-17 13:36:54 +0530

    Update tissot_script.sh

--- tissot_script.sh
+++ tissot_script.sh
@@ -4,24 +4,24 @@ echo Simple script to fetch all required things for tissot to build!
 echo A13 edition!
 
 echo cloning dt
-git clone https://github.com/Meghthedev/device_xiaomi-tissot -b tiramisu device/xiaomi/tissot
+git clone https://github.com/CorvusRom-devices/device_xiaomi-tissot -b 13 device/xiaomi/tissot
 
 echo done
 echo time for common tree hehe
 
-git clone https://github.com/Meghthedev/device_xiaomi_msm8953-common-1.git -b tiramisu device/xiaomi/msm8953-common
+git clone https://github.com/CorvusROM-devices/device_xiaomi_msm8953-common.git -b 13 device/xiaomi/msm8953-common
 
 echo also done
 echo guess what time it is, vendor time
 
-git clone https://github.com/Meghthedev/vendor_xiaomi_tissot-1 vendor/xiaomi/tissot -b tiramisu
+git clone https://github.com/CorvusROM-devices/vendor_xiaomi_tissot vendor/xiaomi/tissot -b 13
 
 echo cant forget common vendor
 
-git clone https://github.com/Evolution-X-Devices/vendor_xiaomi_msm8953-common vendor/xiaomi/msm8953-common -b snow
+git clone https://github.com/CorvusROM-devices/vendor_xiaomi_msm8953-common vendor/xiaomi/msm8953-common -b 13
 
 echo i should stop talking
 
-git clone https://github.com/Anothermi1/cakeby_kernel kernel/xiaomi/msm8953 -b ROM --depth=1
+git clone https://github.com/CorvusROM-devices/kernel_xiaomi_msm8953 kernel/xiaomi/msm8953 -b NON-OC --depth=1
 
 echo done!
